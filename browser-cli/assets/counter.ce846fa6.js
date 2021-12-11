7e7ce385601fb978e1f178fb48cea633f04995fe
PromiSe#### <vicimpa@ya.ru>
2021-12-11 23:08:42 +0000

    Update pages on 2021-12-11 23:08:42

--- browser-cli/assets/counter.ce846fa6.js
+++
@@ -1,1 +0,0 @@
-import{d as t}from"./delay.2ef41e11.js";const r="Every seconds counter";async function i(...o){this.log("Press Ctrl+C for exit.");let e=0;for(;;)this.log("Count: ",++e),await t(10)}export{r as _description_,i as default};
